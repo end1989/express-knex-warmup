75cb800671e6aaa72ae6d600b437109cb1d11c9e
Eldon Dahlin <end1989@gmail.com>
2018-08-02 09:26:49 -0600

    Upload for Tom

--- app.js
+++ app.js
@@ -1,25 +1,35 @@
 const express = require("express"),
-    bodyParser = require("body-parser"),
+    // bodyParser = require("body-parser"),
     cors = require("cors"),
     port = process.env.PORT || 3000,
     app = express(),
     database = require("./database-connection"),
     queries = require("./queries");
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    // const db = database("students").select();
-    // console.log(db);
-    // // res.json({ db });
-    // res.json(db);
     queries.list().then(student => {
         res.json({ student });
     });
 });
 app.get("/:id", (req, res) => {});
+
+app.get("/student/new", (req, res) => {
+    res.render("form.ejs");
+});
+
+app.post("/student", (request, response, next) => {
+    queries
+        .create(request.body)
+        .then(student => {
+            response.status(201).json({ student });
+        })
+        .catch(next);
+});
 
 module.exports = app;
